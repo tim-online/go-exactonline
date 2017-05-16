5bcb68a5398805f376169828042026cfd20c0513
Leon Bogaert <leon@tim-online.nl>
2017-05-16 17:15:44 +0200

    Don't send empty guid's

--- edm/guid.go
+++ edm/guid.go
@@ -1,8 +1,21 @@
 package edm
 
-import "github.com/satori/go.uuid"
+import (
+	"encoding/json"
+
+	"github.com/satori/go.uuid"
+)
 
 // type GUID uuid.UUID
 type GUID struct {
 	uuid.UUID
+}
+
+func (g *GUID) MarshalJSON() ([]byte, error) {
+	empty := uuid.UUID{}
+	if g.UUID == empty {
+		return json.Marshal(nil)
+	}
+
+	return json.Marshal(g.UUID)
 }
