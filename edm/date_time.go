310b3b76264a378b7c34a80d4dd09fa440bc018a
Leon Bogaert <leon@tim-online.nl>
2017-05-16 17:15:10 +0200

    Don't send empty datetime's

--- edm/date_time.go
+++ edm/date_time.go
@@ -11,6 +11,14 @@ type DateTime struct {
 	time.Time
 }
 
+func (d *DateTime) MarshalJSON() ([]byte, error) {
+	if d.Time.IsZero() {
+		return json.Marshal(nil)
+	}
+
+	return json.Marshal(d.Time)
+}
+
 func (d *DateTime) UnmarshalJSON(text []byte) (err error) {
 	var value string
 	err = json.Unmarshal(text, &value)
