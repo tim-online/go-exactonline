39f2620485cd000f4fe30305c7d44eb08661cf70
Leon Bogaert <leon@tim-online.nl>
2018-07-11 10:41:18 +0200

    Support string -> emd.Double json unmarshalling

--- edm/double.go
+++ edm/double.go
@@ -1,3 +1,31 @@
 package edm
 
+import (
+	"encoding/json"
+	"strconv"
+)
+
 type Double float64
+
+func (d *Double) UnmarshalJSON(text []byte) (err error) {
+	var f float64
+	err = json.Unmarshal(text, &f)
+	if err == nil {
+		*d = Double(f)
+		return nil
+	}
+
+	var s string
+	err = json.Unmarshal(text, &s)
+	if err != nil {
+		return err
+	}
+
+	f, err = strconv.ParseFloat(s, 64)
+	if err != nil {
+		return err
+	}
+
+	*d = Double(f)
+	return nil
+}
